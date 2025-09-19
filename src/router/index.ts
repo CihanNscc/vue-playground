b1d5a1faeca8d431ac37bb48bfe2376556a910d0
CihanNscc <alcincihan@gmail.com>
2025-09-19 18:43:08 -0300

    Page components created. Routing paths added.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/Home.vue'),
+    },
+    {
+      path: '/pokemons',
+      name: 'Pokemons',
+      component: () => import('../views/Pokemons.vue'),
+    },
+    {
+      path: '/pokemon/:id',
+      name: 'Pokemon',
+      component: () => import('../views/Pokemon.vue'),
+    },
+  ],
 })
 
 export default router
